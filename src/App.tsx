0e802ff760a6826520ae08ed4d476a0bebab17c7
campbellkiragu <campbellkiragu@gmail.com>
2025-11-14 14:18:33 +0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,26 @@
-import { AuthProvider } from './contexts/AuthContext';
-import { ToastProvider } from './components/Toast';
 import { Router, Route } from './components/Router';
-import { ProtectedRoute } from './components/ProtectedRoute';
-import { HomePage } from './pages/HomePage';
 import { AdminLoginPage } from './pages/AdminLoginPage';
 import { AdminDashboard } from './pages/AdminDashboard';
+import { HomePage } from './pages/HomePage'; // your home page component
 
 function App() {
   return (
-    <AuthProvider>
-      <ToastProvider>
-        <Router>
-          <Route path="/">
-            <HomePage />
-          </Route>
-          <Route path="/admin/login">
-            <AdminLoginPage />
-          </Route>
-          <Route path="/admin">
-            <ProtectedRoute>
-              <AdminDashboard />
-            </ProtectedRoute>
-          </Route>
-        </Router>
-      </ToastProvider>
-    </AuthProvider>
+    <Router>
+      {/* Home page */}
+      <Route path="/">
+        <HomePage />
+      </Route>
+
+      {/* Admin login page */}
+      <Route path="/admin/login">
+        <AdminLoginPage />
+      </Route>
+
+      {/* Admin dashboard */}
+      <Route path="/admin">
+        <AdminDashboard />
+      </Route>
+    </Router>
   );
 }
 
